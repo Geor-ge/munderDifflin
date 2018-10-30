77313a388d2f586e0c712004f37ad42992c02ae0
George Bayley <mzpmgdkqsrnb@opayq.com>
2018-10-30 17:48:20 -0400

    first js lines

---
+++ main.js
@@ -0,0 +1,14 @@
+let card1 = document.getElementById("xerox");
+let card2 = document.getElementById("hammermill");
+let card3 = document.getElementById("hp");
+let card4 = document.getElementById("boise");
+
+card1.addEventListener("click", flipCard);
+card2.addEventListener("click", flipCard);
+card3.addEventListener("click", flipCard);
+card4.addEventListener("click", flipCard);
+
+function flipCard() {
+  
+
+}
