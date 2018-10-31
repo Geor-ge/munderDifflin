c482c73734e9801159096be3ee867e2a269055bb
George Bayley <mzpmgdkqsrnb@opayq.com>
2018-10-31 13:07:52 -0400

    final touches

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 let driveTruck = document.getElementById("drive-truck");
-let truckText = document.getElementById("truck-text");
 driveTruck.addEventListener("click", driveIntoFrame);
-truckText.addEventListener("click", driveIntoFrame);
 
 
 function driveIntoFrame() {
-  driveTruck.style.transition = "transform 2s ease-in-out"
-  driveTruck.style.transform = "translate(-850px)";
-  truckText.style.display = "inline";
-  truckText.style.transition = "display 5s ease-in 2s";
+  driveTruck.style.transition = "transform 1.5s ease-in";
+  driveTruck.style.transform = "rotate(20deg) translate(-850px)";
 }
